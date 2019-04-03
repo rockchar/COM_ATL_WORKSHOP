e2685fb41c2427791d8415fed3ad36b988ac52d3
rohit <rohitsinha050481@gmail.com>
2019-04-03 11:51:40 +0530

    Interface versioning

---
+++ COM_WORKSHOP/InterfaceVersioning/InterfaceDefinitions.h
@@ -0,0 +1,5 @@
+//versioning published interfaces
+/* once an interface is published we cannot change it in any way. But what is a good path foreward for extending the interfaces.
+The standard way to extend an interface is to derive a new interface from an existing one. This technique is called interface inheritance.
+
+*/
---
+++ COM_WORKSHOP/InterfaceVersioning/InterfaceVersioning.cpp
@@ -0,0 +1,11 @@
+// InterfaceVersioning.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
